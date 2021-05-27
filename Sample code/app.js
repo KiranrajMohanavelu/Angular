afe22022e2933e5412de2e0ec9c2142c829642b3
RedCutletWorks <55348215+RedCutletWorks@users.noreply.github.com>
2021-05-27 11:03:33 +0530

    Nothing

--- Sample code/app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.firstname = "Kiranraj";
-    $scope.sayHello = function() {
-        return "Hello Coursera!";
-    }; 
-});
